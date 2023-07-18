52870c7b5149a59f4a525b20fccfa427254d4964
Sulemanshaik013 <Sulemanshaik013@gmail.com>
2023-07-18 23:05:28 +0530

    added structure and pojo classes

---
+++ src/main/java/com/hypothesis/cms/dto/AuthorDTO.java
@@ -0,0 +1,19 @@
+package com.hypothesis.cms.dto;
+public class AuthorDTO {
+
+	private Integer authorId;
+	private int age;
+	public Integer getAuthorId() {
+		return authorId;
+	}
+	public void setAuthorId(Integer authorId) {
+		this.authorId = authorId;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
---
+++ src/main/java/com/hypothesis/cms/dto/CategoriesDTO.java
@@ -0,0 +1,14 @@
+package com.hypothesis.cms.dto;
+public class CategoriesDTO {
+	private String  categoryType;
+
+	public String getCategoryType() {
+		return categoryType;
+	}
+
+	public void setCategoryType(String categoryType) {
+		this.categoryType = categoryType;
+	}
+	
+
+}
