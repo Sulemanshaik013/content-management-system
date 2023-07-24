b8331d2e8ddfed0912c11b1e17120f73b7f756ea
Sulemanshaik013 <Sulemanshaik013@gmail.com>
2023-07-25 01:15:27 +0530

    added exception

---
+++ src/main/java/com/hypothesis/cms/exception/CustomException.java
@@ -0,0 +1,20 @@
+package com.hypothesis.cms.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST,value = HttpStatus.BAD_REQUEST)
+public class CustomException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public CustomException() {
+		super();
+	}
+
+	public CustomException(String message) {
+		super(message);
+	}
+
+	
+}
