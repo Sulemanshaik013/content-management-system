238e7ee49b00cf48d5f1419def79579e681de53c
Sulemanshaik013 <Sulemanshaik013@gmail.com>
2023-07-27 01:39:48 +0530

    added openapi documentation

--- src/main/java/com/hypothesis/cms/ContentManagementSystemApplication.java
+++ src/main/java/com/hypothesis/cms/ContentManagementSystemApplication.java
@@ -3,7 +3,11 @@ package com.hypothesis.cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "cms", version = "1.0", description = "Content Management System"))
 public class ContentManagementSystemApplication {
 
 	public static void main(String[] args) {
